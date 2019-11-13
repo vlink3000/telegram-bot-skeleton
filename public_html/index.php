da64601f89a26041ff7dee4f345c59d0da43d55d
Volodymyr Khorolskyi <Volodymyr.Khorolskyi@stepstone.com>
2019-11-13 15:56:43 +0100

    rm fe part

--- public_html/index.php
+++ public_html/index.php
@@ -6,30 +6,27 @@ require_once ('autoload.php');
 
 use App\Factory\StrategyFactory;
 use App\Http\Curl\CallTelegramApi;
-use App\Auth\Login;
 
-if (isset($_POST['isLogin'])) {
+$file = 'stat.txt';
+$stat = file_get_contents ($file);
+$newRequest = intval($stat) + 1;
+file_put_contents($file, $newRequest);
 
-    $auth = new Login();
-    $auth->ajaxLogin($_POST['login'], $_POST['password']);
+$fn = fopen("stat.txt","r");
+$result = fgets($fn);
+var_dump( $result );
+fclose($fn);
 
-    return true;
-}
+//get response from telegram api
+$request = json_decode(file_get_contents('php://input'), true);
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    header('Location: https://skeleton-telegram-bot.000webhostapp.com/admin/auth');
-} else {
-    //get response from telegram api
-    $request = json_decode(file_get_contents('php://input'), true);
+//save logs, info about  latest conversation
+file_put_contents( 'bot_logs.txt', file_get_contents('php://input'));
 
-    //save logs, info about  latest conversation
-    file_put_contents( 'bot_logs.txt', file_get_contents('php://input'));
+//chose response strategy
+$factory = new StrategyFactory();
+$response = $factory->chooseStrategy($request);
 
-    //chose response strategy
-    $factory = new StrategyFactory();
-    $response = $factory->chooseStrategy($request);
-
-    //send response message to user
-    $curl = new CallTelegramApi();
-    $curl->sendPostRequest($response['method'][0], $response['params']);
-}
+//send response message to user
+$curl = new CallTelegramApi();
+$curl->sendPostRequest($response['method'][0], $response['params']);
